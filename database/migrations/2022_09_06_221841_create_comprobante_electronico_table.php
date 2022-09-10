ab828e6a357a8d4aee07b71b006c3265f84a2bf9
Alexf2019 <antambaalex@gmail.com>
2022-09-09 22:54:08 -0500

    obtener productos

--- database/migrations/2022_09_06_221841_create_comprobante_electronico_table.php
+++ database/migrations/2022_09_06_221841_create_comprobante_electronico_table.php
@@ -17,16 +17,37 @@ class CreateComprobanteElectronicoTable extends Migration
             
             $table->increments('id');
             $table->integer('fecha_emision');
-            $table->integer('tipo_comprobante');
+            //$table->integer('tipo_comprobante');
+            $table->unsignedInteger('tipos_comprobantes_id');
             $table->integer('numero_ruc');
-            $table->integer('tipo_ambiente');
+            //$table->integer('tipo_ambiente');
+            $table->unsignedInteger('tipo_ambiente_id');
             $table->integer('serie');
             $table->integer('numero_comprobante');
             $table->integer('codigo_numerico');
-            $table->integer('tipo_emision');
+            //$table->integer('tipo_emision');
+            $table->unsignedInteger('tipo_emision_id');
             $table->integer('digito_verificador');
             $table->integer('requisito');
             $table->string('estado');
+          
+
+           $table->foreign('tipos_comprobantes_id')
+                ->references('id')
+                ->on('tipos_comprobantes')
+                ->onDelete('cascade');
+
+
+            $table->foreign('tipo_ambiente_id')
+                ->references('id')
+                ->on('tipo_ambiente')
+                ->onDelete('cascade');     
+
+            
+             $table->foreign('tipo_emision_id')
+                ->references('id')
+                ->on('tipo_emision')
+                ->onDelete('cascade');       
             $table->timestamps();
         });
     }
